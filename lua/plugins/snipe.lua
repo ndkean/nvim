ebbf13759ecd87aae4b8371ede322853d38c3387
Nathan Kean <ndkean@gmail.com>
2025-06-23 13:03:14 -0600

    minor snipe change

--- lua/plugins/snipe.lua
+++ lua/plugins/snipe.lua
@@ -10,9 +10,14 @@ return {
     },
   },
   opts = {
+    ui = {
+      max_height = 9,
+    },
     hints = {
       dictionary = '123456789',
     },
     sort = 'last',
   },
 }
+
+-- vim: ts=2 sts=2 sw=2 et
