a6b6c0d056347a7f349ca02e0accedae8327a235
Nathan Kean <ndkean@gmail.com>
2025-07-24 22:29:02 -0500

    Add toggle for line number and relative line number

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -19,6 +19,13 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>tn', function()
+  vim.o.number = not vim.o.number
+end, { desc = '[T]oggle line number' })
+vim.keymap.set('n', '<leader>tr', function()
+  vim.o.relativenumber = not vim.o.relativenumber
+end, { desc = '[T]oggle line number' })
+
 vim.keymap.set('v', '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
 vim.keymap.set('v', '>', '>gv', { desc = 'Indent line' })
 vim.keymap.set('v', '<', '<gv', { desc = 'Uindent line' })
