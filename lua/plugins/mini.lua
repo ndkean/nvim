bf9e1cc2e75622024edb61c5a936afcbfbfb9141
Nathan Kean <ndkean@gmail.com>
2025-09-18 19:45:34 -0500

    Add alt-x as an alternative to deleting a buffer

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,3 +1,18 @@
+local function delete_buffer()
+    local bd = require('mini.bufremove').delete
+    if vim.bo.modified then
+        local choice = vim.fn.confirm(('Save changes to %q?'):format(vim.fn.bufname()), '&Yes\n&No\n&Cancel')
+        if choice == 1 then -- Yes
+            vim.cmd.write()
+            bd(0)
+        elseif choice == 2 then -- No
+            bd(0, true)
+        end
+    else
+        bd(0)
+    end
+end
+
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
   config = function()
@@ -35,25 +50,8 @@ return { -- Collection of various small independent plugins/modules
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
   keys = {
-    {
-      '<leader>bd',
-      function()
-        local bd = require('mini.bufremove').delete
-        if vim.bo.modified then
-          local choice = vim.fn.confirm(('Save changes to %q?'):format(vim.fn.bufname()), '&Yes\n&No\n&Cancel')
-          if choice == 1 then -- Yes
-            vim.cmd.write()
-            bd(0)
-          elseif choice == 2 then -- No
-            bd(0, true)
-          end
-        else
-          bd(0)
-        end
-      end,
-      desc = 'Delete Buffer',
-    },
-				-- stylua: ignore
-				{ "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+    { '<leader>bd', delete_buffer, desc = 'Delete Buffer', },
+    { '<M-x>', delete_buffer, desc = 'Delete Buffer'},
+    { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
   },
 }
